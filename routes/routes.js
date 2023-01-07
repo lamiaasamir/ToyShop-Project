cf9b171273dc9b1b87c9a17bc361a79776a4485c
lamiaasamir <lamiiaasamiir@gmail.com>
2023-01-07 02:54:23 +0200

    modified:   routes/routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const router = express.Router();
-
+const mysql = require('mysql2');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'shop'
+  });
 //import controllers
 const ItemsController = require('../controllers/ItemsController');
 
@@ -21,8 +27,49 @@ router.get('/products', ItemsController.filterProducts);
 router.get('/cart/update/:product', ItemsController.updateCart);
 
 
+// This route allows the user to search for items by name, brand, or price range
 
+router.get('/items', async(req, res) => {
+    // Get the query parameters from the request
+    const { searchValue, searchType} = req.query;
+  
+    // Construct the WHERE clause of the SQL query
+    let whereClause = '';
+    let params = [];
+    if (searchType=="name" ){
+        whereClause += ' AND name LIKE ?';
+        params.push(`%${searchValue}%`);
+    }
+    if (searchType=="brand" ){
+        whereClause += ' AND brand LIKE ?';
+        params.push(`%${searchValue}%`);
+    }
+    
+    if (searchType=="minPrice" ){
+        whereClause += ` AND price >= ${Float.parseFloat(searchValue)}?`; //? needed?
+        params.push(searchValue);
+    }
 
+    
+    if (searchType=="maxPrice" ){
+        whereClause += ` AND price <= ${Float.parseFloat(searchValue)}?`; //? needed ?
+        params.push(searchValue);
+
+    }
+  
+    const brands = await sqlQuery('SELECT brand, image, COUNT(*) as count FROM items GROUP BY brand');
+    // Execute the SQL query using prepared statements
+    connection.query(`SELECT * FROM items WHERE 1=1${whereClause}`, params, (error, results) => {
+      if (error) {
+        res.status(500).send({ error: 'Error querying the database' });
+        return;
+      }
+      res.render('items/products', { items:results, brands});
+      
+      
+    });
+    
+  });
 
 
 module.exports = router;
